a3e61e24be01e1ff6773ea1b29f070c771d2d6b5
Julia <julia.rogovaja@gmail.com>
2022-08-30 20:51:16 +0300

    upd1

---
+++ Example001_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int number = int.Parse(Console.ReadLine());
+
+string? string1 = Console.ReadLine();
+int number = 0;
+if (string1 != null)
+{
+    number = int.Parse(string1);
+}
+else
+{
+    System.Console.WriteLine("Введите число");
+}
+
+
+//Решение математическим путем
+int sqr = number * number;
+System.Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+
+//Решение с использованием библиотеки
+
+int sqr2 = Convert.ToInt32(Math.Pow(number, 2));
+System.Console.WriteLine($"Квадрат числа {number} равен {sqr2}");
---
+++ Example001_Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Задача 7: Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("insert number three-digit number:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number % 10);
