fe8818ac5f354f2d960a19ad46289860df686b23
Julia <julia.rogovaja@gmail.com>
2022-08-30 21:13:45 +0300

    HW_1

---
+++ C#_HW_001_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("insert 1-st number :  ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("insert 2-nd number:  ");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.Write("insert 3-th number:  ");
+int e = Convert.ToInt32(Console.ReadLine());
+int max = c;
+
+if (max < d)
+{
+    max = d;
+}
+if (max < e)
+{
+    max = e;
+}
+
+Console.WriteLine("Max number is :  " + max);
+
---
+++ C#_HW_001_6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и 
+//выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("insert number:  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine(x + " is even number");
+}
+else
+{
+    Console.WriteLine(x + " is not even number");
+}
