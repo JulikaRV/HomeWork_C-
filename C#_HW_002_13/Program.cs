98a8ff1b7c507e9b02e7b46d3b4cd2d6158c137f
Julia <julia.rogovaja@gmail.com>
2022-08-31 19:28:26 +0300

    HW002

---
+++ C#_HW_002_13/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(number > 99 ? number.ToString()[2] : "третьей цифры нет");
+
+
+int getDigit(int k)
+{
+    while (k >= 1000) k = k / 10;
+    int n = k % 10;
+    return n;
+}
+
+
+Console.WriteLine("Введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+int newDigit = 0;
+
+if (digit > 100)
+{
+    newDigit = getDigit(digit);
+    Console.WriteLine(newDigit);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
---
+++ C#_HW_002_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case 1: Console.WriteLine("Понедельник - это рабочий день"); break;
+    case 2: Console.WriteLine("Вторник - это рабочий день"); break;
+    case 3: Console.WriteLine("Среда - это рабочий день"); break;
+    case 4: Console.WriteLine("Четверг - это рабочий день"); break;
+    case 5: Console.WriteLine("Пятница - это рабочий день"); break;
+    case 6: Console.WriteLine("Суббота - это выходной день"); break;
+    case 7: Console.WriteLine("Воскресенье - это выходной день"); break;
+    default: Console.WriteLine("Нет такого дня недели"); break;
+}
